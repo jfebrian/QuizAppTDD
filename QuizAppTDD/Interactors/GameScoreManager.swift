c7dec5c8ef54a6813b45f7accdd41804c9d4136b
hafizh <hafizh.hizb@gmail.com>
2023-01-29 00:01:19 +0700

    first test case created

--- QuizAppTDD/Interactors/GameScoreManager.swift
+++ QuizAppTDD/Interactors/GameScoreManager.swift
@@ -4,5 +4,28 @@
 import Foundation
 
 final class GameScoreManager: ObservableObject {
+    var score = 0
+    var comboBonus = 0
+    var comboMultiplier = 0
     
+    func startGame() {
+        
+    }
+    
+    func selectCorrectAnswer() {
+        score += 10
+        comboBonus += comboMultiplier * 5
+        comboMultiplier += 1
+    }
+    
+    func selectWrongAnswer() {
+        score -= 5
+        score += comboBonus
+        comboBonus = 0
+        comboMultiplier = 0
+    }
+    
+    func endGame() {
+        
+    }
 }
--- QuizAppTDDTests/GameScoreManagerTest.swift
+++ QuizAppTDDTests/GameScoreManagerTest.swift
@@ -6,37 +6,118 @@ import XCTest
 @testable import QuizAppTDD
 
 final class GameScoreManagerTest: XCTestCase {
-
+    let sut = GameScoreManager()
+    
     ///    Given: Player is not playing a game
     ///    When: Player starts a game
     ///    Then: Player initial score, combo bonus, and combo multiplier set to 0
-
+    func testStartGame_initialScore_comboBonus_comboMultiplier_sholdSetZero() {
+        sut.startGame()
+        
+        XCTAssertEqual(sut.score, 0)
+        XCTAssertEqual(sut.comboBonus, 0)
+        XCTAssertEqual(sut.comboMultiplier, 0)
+    }
+    
     ///    Given: Player is playing a game
     ///    When: Player selects a correct answer
     ///    Then: Player score increases by 10
+    func testSelectsACorrectAnswer_scoreIncreasesByTen_whenPlayerIsPlayingAGame() {
+        sut.startGame()
+        
+        sut.selectCorrectAnswer()
+        
+        XCTAssertEqual(sut.score, 10)
+    }
+    
+    ///    Given: Player is playing a game and selected correct answer
+    ///    When: Player selects another correct answer
+    ///    Then: Player score increases again by 10
+    func testSelectAnotherCorrectAnswer_scoreIncreasesAgainByTen_whenSelectedCorrectAnswer() {
+        sut.startGame()
+        sut.selectCorrectAnswer()
+        
+        sut.selectCorrectAnswer()
+        XCTAssertEqual(sut.score, 20)
+    }
     
     ///    Given: Player is playing a game
     ///    When: Player selects correct answer
     ///    Then: Player combo bonus increases by 5 times combo multiplier
+    func testSelectCorrectAnswer_comboBonusIncreasesByFiveTimesComboMultiplier() {
+        sut.startGame()
+        sut.selectCorrectAnswer()
+        XCTAssertEqual(sut.comboBonus, 0)
+        
+        sut.selectCorrectAnswer()
+        XCTAssertEqual(sut.comboBonus, 5)
+    }
     
     ///    Given: Player selected a correct answer
     ///    When: Player combo bonus already increase
     ///    Then: Player combo multiplier increases by 1
-
+    func testSelectCorrectAnswer_comboMultiplierIncreasesByOne() {
+        sut.startGame()
+        sut.selectCorrectAnswer()
+        
+        XCTAssertEqual(sut.comboMultiplier, 1)
+    }
+    
+    ///    Given: Player combo multiplier is 1
+    ///    When: Player selected a correct answer
+    ///    Then: Player combo multiplier increases to 2
+    func testSelectCorrectAnswer_comboMultiplierIncreasesToTwo() {
+        sut.startGame()
+        sut.comboMultiplier = 1
+        
+        sut.selectCorrectAnswer()
+        
+        XCTAssertEqual(sut.comboMultiplier, 2)
+    }
+    
     ///    Given: Player is playing a game
     ///    When: Player selects a wrong answer
     ///    Then: Player score decreases by 5
-
+    func testSelectWrongAnswer_scoreDecreasesByFive() {
+        sut.startGame()
+        sut.selectWrongAnswer()
+        
+        XCTAssertEqual(sut.score, -5)
+    }
+    
     ///    Given: Player selected correct answer
     ///    When: Player selects a wrong answer
     ///    Then: Player score increases by combo bonus
-
+    func testSelectWrongAnswer_scoreIncreasesByComboBonus_whenSelectedCorrectAnswer() {
+        sut.startGame()
+        sut.selectCorrectAnswer()
+        
+        sut.selectWrongAnswer()
+        
+        XCTAssertEqual(sut.score, 5)
+    }
+    
     ///    Given: Player selected correct answer
     ///    When: Player selects a wrong answer
     ///    Then: Player combo bonus and multiplier resets to 0
-
+    func testSelectWrongAnswer_comboBonusAndMultiplierResetsToZero() {
+        sut.startGame()
+        sut.selectCorrectAnswer()
+        
+        sut.selectWrongAnswer()
+        
+        XCTAssertEqual(sut.comboBonus, 0)
+        XCTAssertEqual(sut.comboMultiplier, 0)
+    }
+    
     ///    Given: Player is playing a game
     ///    When: Player ends the game
     ///    Then: Player score added by the current combo bonus
+    func testEndsGame_scoreAddedByCurrentComboBonus() {
+        sut.startGame()
+        sut.endGame()
+        
+        XCTAssertEqual(sut.score, 0)
+    }
 
 }
